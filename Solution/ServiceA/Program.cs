54a15b98fb3c0069bfdf960997ec82ac0431c279
psmon <psmon@live.co.kr>
2017-02-07 23:15:46 +0900

    remote test

---
+++ Solution/ServiceA/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
+using Akka.Event;
+using Akka.Configuration;
+
+
+namespace ServiceA
+{
+    public class MyActor : ReceiveActor
+    {        
+        public MyActor()
+        {
+            Receive<string>(message => {                
+                Console.WriteLine("Received String message: {0}", message);
+                Sender.Tell("re:"+message);
+            });
+
+            Receive<List<int>>(message => {
+                Console.WriteLine("Received ListCount: {0}", message.Count);
+                Sender.Tell("re:" + message);
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+            akka {
+                actor {
+                    provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                }
+
+                remote {
+                    helios.tcp {
+                        port = 9000
+                        hostname = localhost
+                    }
+                }
+            }
+            ");
+
+            using (ActorSystem system = ActorSystem.Create("MyServer", config))
+            {
+                system.ActorOf<MyActor>("greeter");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ Solution/ServiceB/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Akka;
+using Akka.Actor;
+using Akka.Event;
+using Akka.Configuration;
+
+namespace ServiceB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = ConfigurationFactory.ParseString(@"
+            akka {
+                actor {
+                    provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+                }
+
+                remote {
+                    helios.tcp {
+                        port = 9000
+                        hostname = localhost
+                    }
+                }
+            }
+            ");
+
+            using (var system = ActorSystem.Create("MyClient", config))
+            {
+                //get a reference to the remote actor
+                var greeter = system
+                    .ActorSelection("akka.tcp://MyServer@localhost:9000/user/greeter");
+                //send a message to the remote actor
+                
+                
+                var result = greeter.Ask("Hi2", null).Result;
+                Console.WriteLine(result);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    greeter.Tell("Hi..." + i);
+
+                }
+
+                List<int> tmpList = new List<int>();
+                tmpList.Add(1);
+                tmpList.Add(2);
+                tmpList.Add(3);
+
+                greeter.Tell(tmpList);
+                
+                Console.ReadLine();
+            }
+        }
+    }
+}
